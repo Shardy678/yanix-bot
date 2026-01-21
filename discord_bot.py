806af3b18b2ba351da1ec496030dd839788129fb
dabmetal <gyrosuke1@mail.ru>
2026-01-21 13:46:45 +0300

    Коммит ответ на hello и логирование текста

--- discord_bot.py
+++ discord_bot.py
@@ -2,26 +2,43 @@
 
 import os
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
+# class MyClient(discord.Client):
+#     async def on_ready(self):
+#         print(f'Logged on as {self.user}!')
 
-    async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
-        if message.author.bot:
-            return
-        if message.content.startswith("!"):
-            if message.content == "!hello":
-                await message.channel.send('Hello') 
+#     async def on_message(self, message):
+#         print(f'Message from {message.author}: {message.content}')
+#         if message.author.bot:
+#             return
+#         if message.content.startswith("!"):
+#             if message.content == "!hello":
+#                 await message.channel.send('Hello') 
                 
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = MyClient(intents=intents)
-client.run(TOKEN)
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+@bot.event
+async def on_message(message):
+    author = message.author
+    channel = message.channel
+    content = message.content
+
+    print(f'{channel}   {author}: {content}')
+    await bot.process_commands(message)
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello")
+
+
+bot.run(TOKEN)
